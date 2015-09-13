101220a4258246e02a062f0388a5472080ad153d
ch710798472 <710798472@qq.com>
2015-09-13 17:08:49 +0800

    first commit

---
+++ cform_config.php
@@ -0,0 +1,9 @@
+<?php
+ error_reporting(0);
+  
+  // configuration
+  $page_title = "Contact Us Form";
+  $email_it_to = "710798472@qq.com"; //change to your email here
+  $error_message = "Please complete the form first";
+  $confirmation = "Thank you, your message has been successfully sent.";
+?>
